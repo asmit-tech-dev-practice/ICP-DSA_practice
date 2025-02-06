5cbc16b37a872c3d59019eef80b2db9a649547db
Asmit <asmit.banik@gmail.com>
2025-02-06 22:31:22 +0530

    new solutions

--- Solution.java
+++ Solution.java
@@ -1,45 +1,27 @@
 public class Solution {
-    public static int[] mergedArr(int[] firstArr, int[] secondArr) {
-        int i = 0; 
-        int j = 0; 
-        int k = 0; 
-        
-        int[] merged = new int[firstArr.length + secondArr.length];
-
-        while (i < firstArr.length && j < secondArr.length) {
-            if (firstArr[i] < secondArr[j]) {
-                merged[k] = firstArr[i]; 
-                i++;
-            } else {
-                merged[k] = secondArr[j]; 
-                j++;
+    public int countInversions(int[] A) {
+         int count = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] > A[j]) {
+                    count++;
+                }
             }
-            k++; 
         }
-        
-        while (i < firstArr.length) {
-            merged[k] = firstArr[i]; 
-            i++;
-            k++;
-        }
+        return count;
 
-        while (j < secondArr.length) {
-            merged[k] = secondArr[j]; 
-            j++;
-            k++;
-        }
-
-        return merged;
     }
 
-    public static void main(String[] args) {
-        int[] arr1 = {1, 3, 5, 7};
-        int[] arr2 = {2, 4, 6, 8};
+    public int ReversePairs(int[] A) {
+        int count = 0;
 
-        int[] result = mergedArr(arr1, arr2);
-
-        for (int num : result) {
-            System.out.print(num + " ");
+        for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] > 2 * A[j]) { 
+                    count++; 
+                }
+            }
         }
+        return count;
     }
 }
