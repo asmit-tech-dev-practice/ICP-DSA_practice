2980b0a0d15b0c50e4d80d15506db17cb6ad11c6
Asmit <asmit.banik@gmail.com>
2025-02-08 14:57:14 +0530

    tower of hanoi updated

--- Solution.java
+++ Solution.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public int countInversions(int[] A) {
          int count = 0;
@@ -23,5 +26,28 @@ public class Solution {
             }
         }
         return count;
+    }
+
+    public int[] grayCode(int A) {
+        List<Integer> result = new ArrayList<>();
+        grayCodeSolve(A, result);
+        int[] pattern = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            pattern[i] = result.get(i);
+        }
+        return pattern;
+    }
+
+    private void grayCodeSolve(int n, List<Integer> result) {
+        if (n == 0) {
+            result.add(0);
+            return;
+        }
+        grayCodeSolve(n - 1, result);
+        int ans = (int) Math.pow(2, n - 1);
+        for (int i = result.size() - 1; i >= 0; i--) {
+            result.add(result.get(i) + ans);
+
+        }
     }
 }
--- recursion.java
+++ recursion.java
@@ -3,11 +3,26 @@ import java.util.*;
 public class recursion {
 
     public static void main(String[] args) {
-        sumOfNNaturalNumbersUsingRecursion(9);
-        factorialUsingRecursion(8);
-        NthFibbonacciUsingRecursion(10);
-        returnSumOfDigitsUsingRecursion(123);
-        isPalindromeUsingRecursion("racecar");
+        // sumOfNNaturalNumbersUsingRecursion(9);
+        // factorialUsingRecursion(8);
+        // NthFibbonacciUsingRecursion(10);
+        // returnSumOfDigitsUsingRecursion(123);
+        // isPalindromeUsingRecursion("racecar");
+
+        TowerOfHanoi(5, 'A', 'B', 'C');
+        int[] rods = { 5, 0, 0 };
+        solveHanoi(5, 'A', 'C', 'B', rods);
+
+        int n = 3;
+        ArrayList<Integer> rodA = new ArrayList<>();
+        ArrayList<Integer> rodB = new ArrayList<>();
+        ArrayList<Integer> rodC = new ArrayList<>();
+
+        for (int i = n; i >= 1; i--) {
+            rodA.add(i);
+        }
+
+        solveHanoi2(n, 'A', 'C', 'B', rodA, rodB, rodC);
 
     }
 
@@ -63,13 +78,13 @@ public class recursion {
 
     public static void TowerOfHanoi(int N, char src, char aux, char dest) {
 
-        if (N == 0) {
+        if (N == 1) {
             return;
         }
 
-        TowerOfHanoi(N, src, dest, aux);
+        TowerOfHanoi(N - 1, src, dest, aux);
         System.out.println(src + "to" + dest);
-        TowerOfHanoi(N, aux, src, dest);
+        TowerOfHanoi(N - 1, aux, src, dest);
     }
 
     // 0, 0, 2
@@ -80,42 +95,97 @@ public class recursion {
     // 1, 1, 2
     // 0, 0, 2
 
-    public int[] grayCode(int A) {
-        List<Integer> result = new ArrayList<>();
-        grayCodeSolve(A, result);
-        int[] pattern = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            pattern[i] = result.get(i);
-        }
-        return pattern;
-    }
+    public static void solveHanoi(int n, char fromRod, char toRod, char auxRod, int[] rods) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from " + fromRod + " to " + toRod);
 
-    private void grayCodeSolve(int n, List<Integer> result) {
-        if (n == 0) {
-            result.add(0);
+            if (fromRod == 'A') {
+                rods[0]--;
+            } else if (fromRod == 'B') {
+                rods[1]--;
+            } else if (fromRod == 'C') {
+                rods[2]--;
+            }
+
+            if (toRod == 'A') {
+                rods[0]++;
+            } else if (toRod == 'B') {
+                rods[1]++;
+            } else if (toRod == 'C') {
+                rods[2]++;
+            }
+
+            printRodSizes(rods);
             return;
         }
-        grayCodeSolve(n - 1, result);
-        int ans = (int) Math.pow(2, n - 1);
-        for (int i = result.size() - 1; i >= 0; i--) {
-            result.add(result.get(i) + ans);
 
+        solveHanoi(n - 1, fromRod, auxRod, toRod, rods);
+
+        System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+
+        if (fromRod == 'A') {
+            rods[0]--;
+        } else if (fromRod == 'B') {
+            rods[1]--;
+        } else if (fromRod == 'C') {
+            rods[2]--;
+        }
+
+        if (toRod == 'A') {
+            rods[0]++;
+        } else if (toRod == 'B') {
+            rods[1]++;
+        } else if (toRod == 'C') {
+            rods[2]++;
+        }
+
+        printRodSizes(rods);
+
+        solveHanoi(n - 1, auxRod, toRod, fromRod, rods);
+    }
+
+    public static void printRodSizes(int[] rods) {
+        System.out.println("Rod Sizes: A=" + rods[0] + ", B=" + rods[1] + ", C=" + rods[2]);
+    }
+
+
+
+
+    public static void solveHanoi2(int n, char fromRod, char toRod, char auxRod, ArrayList<Integer> rodA, ArrayList<Integer> rodB, ArrayList<Integer> rodC) {
+        if (n == 1) {
+            System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+            moveBlock(fromRod, toRod, rodA, rodB, rodC);
+            printRodState(rodA, rodB, rodC);
+            return;
+        }
+        solveHanoi2(n - 1, fromRod, auxRod, toRod, rodA, rodB, rodC);
+
+        System.out.println("Move disk " + n + " from " + fromRod + " to " + toRod);
+        moveBlock(fromRod, toRod, rodA, rodB, rodC);
+        printRodState(rodA, rodB, rodC);
+        solveHanoi2(n - 1, auxRod, toRod, fromRod, rodA, rodB, rodC);
+    }
+
+    public static void moveBlock(char fromRod, char toRod, ArrayList<Integer> rodA, ArrayList<Integer> rodB, ArrayList<Integer> rodC) {
+        if (fromRod == 'A' && toRod == 'B') {
+            rodB.add(rodA.remove(rodA.size() - 1));
+        } else if (fromRod == 'A' && toRod == 'C') {
+            rodC.add(rodA.remove(rodA.size() - 1));
+        } else if (fromRod == 'B' && toRod == 'A') {
+            rodA.add(rodB.remove(rodB.size() - 1));
+        } else if (fromRod == 'B' && toRod == 'C') {
+            rodC.add(rodB.remove(rodB.size() - 1));
+        } else if (fromRod == 'C' && toRod == 'A') {
+            rodA.add(rodC.remove(rodC.size() - 1));
+        } else if (fromRod == 'C' && toRod == 'B') {
+            rodB.add(rodC.remove(rodC.size() - 1));
         }
     }
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static void printRodState(ArrayList<Integer> rodA, ArrayList<Integer> rodB, ArrayList<Integer> rodC) {
+        System.out.println("Rod A: " + rodA);
+        System.out.println("Rod B: " + rodB);
+        System.out.println("Rod C: " + rodC);
+    }
 
 }
